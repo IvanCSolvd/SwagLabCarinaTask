4645960928888ac42c707843cbdc2d8b87d363a8
Ivan Castellanos <icastellanos@solvd.com>
2024-09-12 23:41:27 -0300

    fix: updated locator

--- src/main/java/com/zebrunner/carina/demo/swaglabs/components/ios/IOSFilterComponent.java
+++ src/main/java/com/zebrunner/carina/demo/swaglabs/components/ios/IOSFilterComponent.java
@@ -3,13 +3,13 @@ package com.zebrunner.carina.demo.swaglabs.components.ios;
 import com.zebrunner.carina.demo.swaglabs.components.common.FilterComponent;
 import com.zebrunner.carina.demo.swaglabs.enums.SortingType;
 import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
+import com.zebrunner.carina.webdriver.locator.ExtendedFindBy;
 import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.FindBy;
 
 public class IOSFilterComponent extends FilterComponent {
 
-    @FindBy(xpath = "//android.widget.TextView[@text='%s']")
+    @ExtendedFindBy(iosPredicate = "name == 'test-Modal Selector Button'")
     private ExtendedWebElement filterDropdownBtn;
 
 
